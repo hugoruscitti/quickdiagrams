f5704ad33c7ccdcbb53d18801a93e140a155f0af
Hugo Ruscitti <hugoruscitti@gmail.com>
2010-05-23 22:37:19 -0300

    modificando la biblioteca base para utilizar pygraphviz en lugar de python-yapgvb

--- quickdiagrams/main.py
+++ quickdiagrams/main.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import yapgvb
+import pygraphviz as pgv
 import parser
 import pac_parser
 
@@ -8,7 +8,7 @@ class Diagram:
 
     def __init__(self, disable_visible_warnings=False):
         self.content = []
-        self.digraph = yapgvb.Digraph()
+        self.digraph = pgv.AGraph(strict=False, directed=True)
         self.nodes = {}
         self.digraph.rankdir = "BT"
         self.digraph.nodesep = 1
@@ -21,13 +21,11 @@ class Diagram:
         self.error_messages.append("Cuidado: " + message)
 
         if not self.disable_visible_warnings:
-            node = self.digraph.add_node(message)
-            node.shape = 'record'
-            node.fontsize = 10
-            node.fontname = "Verdana"
-            node.color = "red3"
-            node.fontcolor = "red3"
-            node.label = "cuidado: \n" + message
+            self.digraph.add_node(message, shape='record', color='red3',
+                    fontcolor='red3', fontsize='10', fontname='verdana')
+            #, shape='record', fontsize='10',
+            #    fontname="Verdana", color="red3",
+            #    fontcolor="red3", label="cuidado: \n" + message)
 
     def read(self, input_file_name):
         "Lee un modelo de clases desde un archivo de texto."
@@ -71,27 +69,22 @@ class Diagram:
     def add_model(self, model):
         "Genera un nodo de clase para el modelo indicado."
 
-        node = self.digraph.add_node(str(model.name))
-        node.shape = 'record'
-        node.fontsize = 10
-        node.fontname = "Verdana"
-        node.label = model.get_content_as_string()
-        
+        model_content = model.get_content_as_string()
+        node = self.digraph.add_node(str(model.name), shape='record', fontsize='10',
+                                     fontname='Verdana', 
+                                     label=model_content)
         if model.name:
-            name = model.name.lower()
+            name = model.name
         else:
             name = "??"
 
-        self.nodes[name] = (model.superclass, node)
+        self.nodes[name] = (model.superclass, name)
 
     def add_empty_node(self):
         "Genera un nodo indicando que el modelo está vacío."
 
-        node = self.digraph.add_node("Empty model")
-        node.shape = 'none'
-        node.fontsize = 10
-        node.fontname = "Verdana"
-        #node.fontcolor = 'gray'
+        node = self.digraph.add_node("Empty model", shape='none',
+                                     fontsize='10', fontname="Verdana")
 
     def get_node_from_name(self, name):
         """Obtiene un nodo asociado a un determinado nombre de clase.
@@ -115,11 +108,12 @@ class Diagram:
         for key, value in self.nodes.items():
             superclass, node = value
 
+
             if superclass:
-                superclass_node = self.get_node_from_name(superclass)
-                edge = self.digraph.add_edge(node, superclass_node)
-                edge.arrowhead = 'onormal'
-                edge.arrowtail = 'none'
+                #superclass_node = self.get_node_from_name(superclass)
+                edge = self.digraph.add_edge(node, superclass, 
+                        arrowhead='onormal',
+                        arrowtail = 'none')
 
     def create_explicit_relationships(self, relationships):
         """Genera las relaciones que el usuario indica manualmente.
@@ -135,25 +129,21 @@ class Diagram:
                 from_node = self.get_node_from_name(rel.from_name)
                 to_node = self.get_node_from_name(rel.to_name)
             except KeyError, msg:
-                msg_error = "No se encuentra la clase %s al crear una relación."
+                msg_error = u"No se encuentra la clase %s para dibujar la relación."
                 self.add_warning_message(msg_error %(msg))
                 return
 
-            self.create_relation(from_node, to_node, rel)
+            self.create_relation(rel.from_name, rel.to_name, rel)
 
     def create_relation(self, from_node, to_node, rel):
-        edge = self.digraph.add_edge(from_node, to_node)
-        edge.arrowtail = rel.arrowhead
-        edge.label = rel.description
-        edge.fontsize = 10
-        edge.fontname = "Verdana"
-        edge.taillabel = rel.from_counter
-        edge.headlabel = rel.to_counter
-        edge.arrowhead = rel.arrowtail
+        edge = self.digraph.add_edge(from_node, to_node, arrowtail='arrowhead',
+                label=rel.description, fontsize='10', fontname='Verdana',
+                taillabel=rel.from_counter, headlabel=rel.to_counter, 
+                arrowhead=rel.arrowtail)
 
     def save(self, filename, format, disable_output=False):
-        self.digraph.layout(yapgvb.engines.dot)
-        self.digraph.render(filename, format)
+        self.digraph.layout(prog='dot')
+        self.digraph.draw(filename, format)
 
         if not disable_output:
             print "Creating file: %s" %(filename)
@@ -169,5 +159,19 @@ class Diagram:
 
 if __name__ == '__main__':
     diagram = Diagram()
-    diagram.read_from_string(['Process', '\tpid', '\tkill()'])
+    code = """
+A
+    bb
+
+    SubA
+        a
+
+B
+    no
+
+
+A -> B
+"""
+
+    diagram.read_from_string(code.split('\n'))
     diagram.save('test.png', 'png')
